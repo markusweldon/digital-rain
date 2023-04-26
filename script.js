d39a59f8da4e2b6a7db4433abb80414e1167ee14
Markus Weldon <markus.weldon@gmail.com>
2023-04-26 11:05:37 -0700

    separated concerns of styling to css

--- script.js
+++ script.js
@@ -1,52 +1,50 @@
-// Get a reference to the canvas element and set up its context
+// Get the canvas element and its 2D context
 const canvas = document.getElementById('matrixCanvas');
 const ctx = canvas.getContext('2d');
 
-// Set the canvas size to fill the window
+// Set the canvas size to match the window size
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
-// Set font size and determine the number of columns based on the canvas width
-const fontSize = 10;
+// Get the font size from CSS variable and calculate the number of columns
+const fontSize = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--font-size'));
 const columns = canvas.width / fontSize;
 
-// Define the set of characters used for the Matrix rain effect
+// Define the characters that can fall and split them into an array
 const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$%^&*()_+-=[]{}|;':\",.<>/?";
 const charArray = characters.split('');
 
-// Initialize the drops array, representing the falling text
+// Create an array to keep track of the position of each column
 const drops = [];
 for (let i = 0; i < columns; i++) {
   drops[i] = 1;
 }
 
-// The draw function updates the canvas with new characters for the Matrix rain effect
+// Define the function that renders the rain effect on the canvas
 function draw() {
-  // Set a semi-transparent black background to create a trail effect
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
+  // Add background color with fading effect
+  const backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--background-color').trim();
+  ctx.fillStyle = backgroundColor;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  // Set the text color and font
-  ctx.fillStyle = '#0f0';
+  // Set text color
+  const fontColor = getComputedStyle(document.documentElement).getPropertyValue('--text-color').trim();
+  ctx.fillStyle = fontColor;
   ctx.font = fontSize + 'px monospace';
 
-  // Loop through the drops array, updating each drop's position
+  // Loop through each column and render a character at its position
   for (let i = 0; i < drops.length; i++) {
-    // Randomly select a character from the character array
     const text = charArray[Math.floor(Math.random() * charArray.length)];
-
-    // Draw the character at the drop's current position
     ctx.fillText(text, i * fontSize, drops[i] * fontSize);
 
-    // Reset the drop's position if it reaches the bottom or randomly with a low probability
+    // Check if the character has reached the bottom of the canvas or if a random number condition is met
     if (drops[i] * fontSize > canvas.height || Math.random() > 0.975) {
       drops[i] = 0;
     }
-
-    // Increment the drop's position for the next frame
+    // Increment the position of the character in the column
     drops[i]++;
   }
 }
 
-// Call the draw function repeatedly at 33ms intervals (about 30 FPS)
+// Call the draw() function at a regular interval to create the rain effect on the canvas
 setInterval(draw, 33);
