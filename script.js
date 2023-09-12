8a290235caac5e6ad5e1036822eca793a6eb6906
Markus Weldon <markus.weldon@gmail.com>
2023-09-12 13:52:40 -0700

    added settings function for updating styles

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
 // Get the font size from CSS variable and calculate the number of columns
-const fontSize = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--font-size'));
-const columns = canvas.width / fontSize;
+let fontSize = 10;
+let columns = canvas.width / fontSize;
 
 // Define the characters that can fall and split them into an array
 const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$%^&*()_+-=[]{}|;':\",.<>/?";
@@ -20,7 +20,32 @@ for (let i = 0; i < columns; i++) {
   drops[i] = 1;
 }
 
-// Define the function that renders the rain effect on the canvas
+function updateStyles() {
+  const bgAlpha = document.getElementById('bg-alpha-range').value;
+  const textAlpha = document.getElementById('text-alpha-range').value;
+  const bgColorPicker = document.getElementById('bg-color-picker').value;
+  const textColorPicker = document.getElementById('text-color-picker').value;
+  
+  document.documentElement.style.setProperty('--background-color', `${bgColorPicker}${Math.floor(bgAlpha * 255).toString(16)}`);
+  document.documentElement.style.setProperty('--text-color', `${textColorPicker}${Math.floor(textAlpha * 255).toString(16)}`);
+
+  fontSize = parseInt(document.getElementById('font-size-range').value);
+  document.documentElement.style.setProperty('--font-size', fontSize + 'px');
+  document.getElementById('font-size-value').textContent = fontSize + 'px';
+  columns = canvas.width / fontSize;
+}
+
+document.getElementById('bg-color-picker').addEventListener('input', updateStyles);
+document.getElementById('text-color-picker').addEventListener('input', updateStyles);
+document.getElementById('bg-alpha-range').addEventListener('input', updateStyles);
+document.getElementById('text-alpha-range').addEventListener('input', updateStyles);
+document.getElementById('font-size-range').addEventListener('input', updateStyles);
+
+document.getElementById('settings-toggle-button').addEventListener('click', () => {
+  const settingsPanel = document.getElementById('settings-panel');
+  settingsPanel.style.display = settingsPanel.style.display === 'block' ? 'none' : 'block';
+});
+
 function draw() {
   // Add background color with fading effect
   const backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--background-color').trim();
